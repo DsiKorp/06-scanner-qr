326f65daa6df70684877deb185cd2575ac263ea8
Juan Delgado <dsikorp@gmail.com>
2026-04-27 16:11:59 -0500

    init scan code

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +9,9 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() { }
+  constructor(
+    private barcodeScanner: BarcodeScanner
+  ) { }
 
   ionViewDidEnter() {
     console.log('ionViewDidEnter event fired');
@@ -25,5 +28,13 @@ export class Tab1Page {
   ionViewWillLeave() {
     console.log('ionViewWillLeave event fired');
   };
+
+  scan() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
 
 }
