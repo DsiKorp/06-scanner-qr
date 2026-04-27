5d51cf31b8682787a9f9297d51abe1fa24fa8380
Juan Delgado <dsikorp@gmail.com>
2026-04-27 12:30:44 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '06-scanner-qr',
+  webDir: 'www'
+};
+
+export default config;
